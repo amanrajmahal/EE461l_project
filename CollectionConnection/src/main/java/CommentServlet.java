f649d518abb1beeab6bb81dbdce6abfbde9b2742
unknown <mikalai.shlapkou@utexas.edu>
2018-03-08 16:13:29 -0600

    Project Initialized

---
+++ CollectionConnection/src/main/java/CommentServlet.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CommentServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+	}
+}
---
+++ CollectionConnection/src/main/java/ProfileServlet.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProfileServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+	}
+}
