1ee4d388f1e371b1559bbc0ecb2197721f1dc14d
unknown <mikalai.shlapkou@utexas.edu>
2018-04-09 11:14:29 -0500

    Current Progress
    This excludes the commit with the comment for now. The current issues are: Sometimes, after creating a profile, it asks you to put in your first and last name again after you submit it. Also, the webpage sometimes doesnt refresh properly and it requires manual refresh to see the new photo.

--- src/main/java/collectionconnection/Profile.java
+++ src/main/java/collectionconnection/Profile.java
@@ -1,10 +1,5 @@
 package collectionconnection;
-
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Observable;
-import java.util.Observer;
-import java.util.Set;
 
 import com.google.appengine.api.users.User;
 import com.googlecode.objectify.Key;
@@ -16,120 +11,97 @@ import com.googlecode.objectify.annotation.Parent;
 import java.util.*;
 
 @Entity
-public class Profile extends Observable implements Comparable<Profile>, Observer {
-	@Parent
-	Key<ProfileKey> user;
-	@Id
-	Long id;
-	@Index
-	User actualUser;
-	@Index
-	String firstName;
-	@Index
-	String lastName;
-	@Index
-	Date date;
-	private Set<Profile> subscriptionList;
-	private Notification notificationStyle;
-
-	private ArrayList<Collection> collections;
-
-	private Profile() {
-	}
-
-	public Profile(User user, String firstName, String lastName) {
-		this.user = Key.create(ProfileKey.class, user.getNickname());
-		this.actualUser = user;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.date = new Date();
-		subscriptionList = new HashSet<>();
-		this.notificationStyle = null;
-	}
-	
-	public String getEmail() {
-		return actualUser.getEmail();
-	}
-
-	public User getUser() {
-		return actualUser;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void addToSubscriberList(Profile profile) {
-		subscriptionList.add(profile);
-		//profile.addObserver(o);
-	}
-
-	public void removeFromSubscriberList(Profile profile) {
-		subscriptionList.remove(profile);
-	}
-
-	public void setNotificationStyle(Notification notificationStyle) {
-		this.notificationStyle = notificationStyle;
-	}
-
-	public boolean addCollection(String collectionName) {
-		// quick check for duplicate collections for profile
-		if (collections == null)
-			this.collections = new ArrayList<Collection>();
-		for (Collection collection : collections) {
-			if (collection.getCollectionName().equals(collectionName))
-				return false;
-		}
-		collections.add(new Collection(collectionName));
-		return true;
-	}
-
-	public boolean addPhoto(String collectionName, String name, String blobKey) {
-		// find the collection
-		if (collections == null)
-			this.collections = new ArrayList<Collection>();
-		for (Collection collection : collections) {
-			if (collection.getCollectionName().equals(collectionName)) {
-				collection.addPhoto(name, blobKey);
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public boolean containsCollection(String collectionName) {
-		if (collections == null)
-			this.collections = new ArrayList<Collection>();
-		for (Collection collection : collections) {
-			if (collection.getCollectionName().equals(collectionName)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public ArrayList<Collection> getCollections() {
-		return collections;
-	}
-
-	@Override
-	public int compareTo(Profile other) {
-		if (date.after(other.date)) {
-			return 1;
-		} else if (date.before(other.date)) {
-			return -1;
-		}
-		return 0;
-	}
-
-	@Override
-	public void update(Observable o, Object arg) {
-		if (this.notificationStyle.getClass() == RealTimeNotification.class) {
-			notificationStyle.alert();
-		}
-	}
+public class Profile implements Comparable<Profile>{
+    @Id Long id;
+    @Index User actualUser;
+    
+    private String firstName;
+    private String lastName;
+    private Date date;
+    private ArrayList<Collection> collections;
+    
+    private Profile() {}
+    public Profile(User user, String firstName, String lastName) { 
+    	//this.user = Key.create(ProfileKey.class, user.getNickname());
+    	this.actualUser = user;
+    	this.firstName = firstName;
+    	this.lastName = lastName;
+        this.date = new Date();
+        this.collections = new ArrayList<Collection>();
+    }
+    
+    public User getUser()
+    {
+    	return actualUser;
+    }
+    
+    public String getFirstName()
+    {
+    	return firstName;
+    }
+    
+    public String getLastName()
+    {
+    	return lastName;
+    }
+    
+    public boolean addCollection(String collectionName)
+    {
+    	checkCollections();
+    	for(Collection collection : collections)
+    	{
+    		if(collection.getCollectionName().equals(collectionName)) return false;
+    	}
+    	collections.add(new Collection(collectionName));
+    	return true;
+    }
+    
+    public boolean addPhoto(String collectionName, String name, String blobKey)
+    {
+    	checkCollections();
+    	for(Collection collection : collections)
+    	{
+    		if(collection.getCollectionName().equals(collectionName))
+    		{
+    			collection.addPhoto(name, blobKey);
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    public boolean containsCollection(String collectionName)
+    {
+    	checkCollections();
+    	for(Collection collection : collections)
+    	{
+    		if(collection.getCollectionName().equals(collectionName))
+    		{
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    public ArrayList<Collection> getCollections()
+    {
+    	checkCollections();
+    	return collections;
+    }
+    
+    private void checkCollections()
+    {
+    	if(collections == null) this.collections = new ArrayList<Collection>();
+    }
+    
+    @Override
+    public int compareTo(Profile other) {
+        if (date.after(other.date)) {
+            return 1;
+        } else if (date.before(other.date)) {
+            return -1;
+        }
+        return 0;
+     }
+    
 }
