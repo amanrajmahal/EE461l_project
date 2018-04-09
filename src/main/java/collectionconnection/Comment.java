229e16f9ea8a0654d2cbff3ed3cd44e4dbe2aac6
Ryo Yoshida <ryo.yoshida@utexas.edu>
2018-04-09 14:19:39 -0500

    Date on comment

--- src/main/java/collectionconnection/Comment.java
+++ src/main/java/collectionconnection/Comment.java
@@ -1,14 +1,17 @@
 package collectionconnection;
 
+import java.util.Date;
 import com.google.appengine.api.users.User;
 
 public class Comment {
 	private User user;
 	private String comment;
+	private Date date;
 	
 	public Comment(User user, String comment) {
 		this.user = user;
 		this.comment = comment;
+		this.date = new Date();
 	}
 	
 	public User getUser() {
@@ -17,5 +20,9 @@ public class Comment {
 	
 	public String getComment() {
 		return comment;
+	}
+	
+	public Date getDate() {
+		return date;
 	}
 }
